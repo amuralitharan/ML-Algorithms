008d575a100bbd10fddb55bb346df4a2f2ad827a
amuralitharan <52011905+amuralitharan@users.noreply.github.com>
2020-09-22 13:45:56 -0700

    Cost function

--- linear_regression.py
+++ linear_regression.py
@@ -2,19 +2,23 @@ import os
 import numpy as np
 from matplotlib import pyplot
 
-def load_data():
+def cost(theta, y, X):
+	n = y.size
+	prediction = np.sum(np.square(y - np.inverse(theta[1:])*X + theta[0]))/
+
+def gradient_descent():
+	return 1
+
+def predict():
+	return 1
+
+def main():
+	#load data
 	data = np.genfromtxt(os.path.join('Data', 'winequality-white.csv'), delimiter=';')
 	X, y = data[:, :11], data[:, 11:]
 	print(y.shape)
 
-def cost():
 
-def gradient_descent():
-
-def predict:
-
-def main():
-	load_data()
 
 if __name__ == "__main__":
     main()
